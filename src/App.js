0539a045695fc59794e0154f9e0246413b581725
Shakil00077 <shakilshehjaad@gmail.com>
2022-03-20 17:01:25 +0600

    added 10 user data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadUsers></LoadUsers>
     </div>
   );
+}
+
+function LoadUsers() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+      console.log(users);
+  }, ([]))
+
+
+  return (
+    <div>
+      <h1>User: {users.length}</h1>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+
+function User(props) {
+  return (
+    <div className='users'>
+      <h3>Name: {props.name}</h3>
+      <h5>Email: {props.email}</h5>
+    </div>
+  )
 }
 
 export default App;
